d4dc94b38af8158bf35430b77c2e95a864afc414
srmanry402 <sumanry402@gmail.com>
2025-07-13 06:23:53 +0300

    last Commit

--- lib/helper/route_helper.dart
+++ lib/helper/route_helper.dart
@@ -7,18 +7,12 @@ import 'package:get/get.dart';
 class RouteHelper {
   static const String initial = '/';
   static const String splash = '/splash';
-  static const String onboarding = '/onboarding';
 
 
   static getInitialRoute() => splash;
   static getSplashRoute() => splash;
-  static getOnboardingRoute() => onboarding;
-
 
   static List<GetPage> routes = [
-  /*  GetPage(name: initial, page: () => const SplashScreenView()),
-    GetPage(name: splash, page: () => const SplashScreenView()),*/
-
 
   ];
 }
